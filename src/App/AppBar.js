2edf909581648d5ca60b1d55a822a6acb99fa792
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2019-09-10 12:56:27 +0900

    [update] add AppBar

---
+++ src/App/AppBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Bar = styled.div`
+  display: grid;
+`
+const AppBar = () => (
+  <Bar> Hello </Bar>
+)
+
+export default AppBar
