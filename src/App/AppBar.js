fd13353370618e32c681c72b2d6d0b6acef42a1c
田原 一樹 <ssp0727.lnc@gmail.com>
2019-09-11 00:38:13 +0900

    [update]

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,11 +1,36 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`
 
 const Bar = styled.div`
   display: grid;
+  grid-template-columns: 180px auto 180px 100px;
 `
+
+const ControlButtonElem = styled.div`
+  cusour: pointer;
+  ${props => props.actice && css`
+    color: blue
+  `}
+`
+
+const ControlButton = ({ name, active }) => (
+  <ControlButtonElem active={active}>
+    {name}
+  </ControlButtonElem>
+)
+
+
 const AppBar = () => (
-  <Bar> Hello </Bar>
+  <Bar>
+    <Logo> CryptoDash </Logo>
+    <div />
+    <ControlButton active name='dashboard'/>
+    <ControlButton name='setting'/>
+  </Bar>
 )
 
 export default AppBar
