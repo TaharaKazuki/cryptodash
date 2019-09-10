35a9974c2fae5a2783a40974daca02eecba8b74e
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2019-09-10 12:48:40 +0900

    [update] styled-component

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import AppLayout from './AppLayout'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">Hello World</div>  
+      <AppLayout>
+        <div className="App">Hello World</div>  
+      </AppLayout>
     )
   }
 }
