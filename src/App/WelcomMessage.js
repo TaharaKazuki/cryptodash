323dfd45140b3d5237c68e261d7ce9906d106dd9
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2019-09-14 20:50:42 +0900

    [update]

---
+++ src/App/WelcomMessage.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const WelcomMessage = () => (
+  <h1>WelcomMessage</h1>
+)
+export default WelcomMessage
--- src/App/index.js
+++ src/App/index.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import AppLayout from './AppLayout'
 import AppBar from './AppBar'
+import WelcomMessage from './WelcomMessage'
+
 
 class App extends Component {
   render () {
     return (
       <AppLayout>
+        <WelcomMessage />
         <AppBar />
         <div className="App">Hello World</div>  
       </AppLayout>
     )
   }
 }
-
 export default App;
+
